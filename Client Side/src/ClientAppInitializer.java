ab13bdefe462b1e8025165391fbfe3c2d5923c35
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-08-10 11:12:05 +0530

    Create AppInitializer Class

---
+++ Client Side/src/ClientAppInitializer.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : Chat Application
+ * @date : 8/10/2022
+ * @month : 08
+ * @year : 2022
+ * @since : 0.1.0
+ **/
+public class ClientAppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("ClientForm.fxml"))));
+        primaryStage.show();
+    }
+}
---
+++ Client Side/src/controller/ClientFormController.java
@@ -0,0 +1,12 @@
+package controller;
+
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : Chat Application
+ * @date : 8/10/2022
+ * @month : 08
+ * @year : 2022
+ * @since : 0.1.0
+ **/
+public class ClientFormController {
+}
